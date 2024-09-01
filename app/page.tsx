4926951d5fbc2238aa7b1e0afdb8b6f0c43bce84
Fernando Gustavo <89613568+DeveloperFG@users.noreply.github.com>
2024-09-01 10:36:53 -0300

    segundo commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+
+  return (
+    <main className="flex w-100% h-100% items-center bg-black  ">
+      <div>
+          <h1 className="text-white"> Bem vindo: Loja do Povo </h1>
+          <span>Fernando Gustavo</span>
+      </div>
+    </main>
+  );
+}
